7cc3888680b3ed7df4ab28ba55aacef4ae8f3c7c
Prashanth <87235698+dpr2106@users.noreply.github.com>
2026-07-19 21:09:43 +0530

    Initialization of creating a custom python library

---
+++ src/hornetcodes/from hornetcodes import my_custom_comman.py
@@ -0,0 +1,5 @@
+import hornetcodes as bund
+bund.add_numbers()
+
+
+print("This sentence will only appear if the command has been successfully executed.")
---
+++ src/hornetcodes/my_custom_commands.py
@@ -0,0 +1,8 @@
+def add_numbers():
+    print("Enter the first number:" , end = " ")
+    num1 = int(input())
+    print("Enter the second number:" , end = " ")
+    num2 = int(input())
+    print("The sum of", num1, "and", num2, "is", num1 + num2)
+
+
